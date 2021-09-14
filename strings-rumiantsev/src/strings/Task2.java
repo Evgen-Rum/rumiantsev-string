8f2bed942f3eb2c60189e85d49f31b469d10856c
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-09-14 15:12:44 +0300

    create new project

---
+++ strings-rumiantsev/src/strings/Task2.java
@@ -0,0 +1,16 @@
+package strings;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Task2 {
+	public static void main(String[] args) {
+		String inp = "Hello my black kitten. My black kitten is very beautiful. I really love black kittens! ";
+		Pattern pattern = Pattern.compile("black(\\w*)");
+		Matcher matcher = pattern.matcher(inp);
+		String newStr = matcher.replaceAll("white");
+		System.out.println(newStr);
+	}
+
+}
