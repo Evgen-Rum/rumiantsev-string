e19334f1450c07462502511cf5c52e46845b108c
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-09-14 15:33:44 +0300

    format

--- strings-rumiantsev/src/strings/Task2.java
+++ strings-rumiantsev/src/strings/Task2.java
@@ -3,7 +3,6 @@ package strings;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class Task2 {
 	public static void main(String[] args) {
 		String inp = "Hello my black kitten. My black kitten is very beautiful. I really love black kittens! ";
