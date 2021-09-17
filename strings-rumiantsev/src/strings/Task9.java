e2ab70a40a831a1920ec239750ee08e89bfec24b
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-09-17 13:14:09 +0300

    Added user interaction and incorrect value verification

--- strings-rumiantsev/src/strings/Task9.java
+++ strings-rumiantsev/src/strings/Task9.java
@@ -1,15 +1,32 @@
 package strings;
 
+import java.util.Scanner;
+
 public class Task9 {
 	public static void main(String[] args) {
-		String str = "Nissan x-trail, xor beginner, xXx";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter some line.");
+		String str = sc.nextLine();
+		System.out.println("Please enter the number of letter witch you find.");
+		int numOfLetter = 0;
+		for (int i = 0; i < 3; i++) {
+			if (sc.hasNextInt()) {
+				numOfLetter = sc.nextInt();
+				break;
+			} else if (i < 2){
+				System.out.println("Incorrect data! Please enter whole number!");
+				sc.next();
+			}  else {
+				System.out.println("Inccorect data! End of the program!");
+			}
+		}
 		int count = 0;
 		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) == 'x') {
+			if (str.charAt(i) == numOfLetter) {
 				count++;
 			}
 		}
-		System.out.println("Number of x letters in line: " + count);
+		System.out.println("Number of x letters in your line: " + count);
 	}
 
 }
