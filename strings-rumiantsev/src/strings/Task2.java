2e40f7f32b7368bf50497be587144eab52bae4aa
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-10-24 15:20:08 +0300

    fixed regular expression. Added String[]

--- strings-rumiantsev/src/strings/Task2.java
+++ strings-rumiantsev/src/strings/Task2.java
@@ -5,11 +5,15 @@ import java.util.regex.Pattern;
 
 public class Task2 {
 	public static void main(String[] args) {
-		String inp = "Hello my black kitten. My black kitten is very beautiful. I really love black kittens! ";
-		Pattern pattern = Pattern.compile("black(\\w*)");
-		Matcher matcher = pattern.matcher(inp);
-		String newStr = matcher.replaceAll("white");
-		System.out.println(newStr);
+		String[] inp = {
+				"Hello my bLAck kitten. My BlacK kitten is very beautiful. I really love BLACK kittens! blackage. ",
+				"BlAcK", "BLACKAGED, blackBlack" };
+		for (int i = 0; i < inp.length; i++) {
+			Pattern pattern = Pattern.compile("(?i)\\bblack\\b");
+			Matcher matcher = pattern.matcher(inp[i]);
+			String newStr = matcher.replaceAll("white");
+			System.out.println(newStr);
+		}
 	}
 
 }
